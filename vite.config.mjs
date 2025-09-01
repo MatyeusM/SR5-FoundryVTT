1a74e067ecf2cd80dd69631a83c8cf07532b44fa
MatyeusM <matthias.mandelartz@googlemail.com>
2025-09-01 11:39:31 +0200

    Replace custom FoundryVTT plugin with vite-plugin-fvtt
    - Remove local vite-plugin-foundryvtt and related dependencies.

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import foundryVTT from './build/vite-plugin-foundryvtt/index.mjs';
+import foundryVTT from 'vite-plugin-fvtt';
 
 export default defineConfig({
     // TODO @taMif: remove this line before compiling the release with it. never add it again.
